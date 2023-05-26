768af31f1aaaeaa21b9e66da82a30cd6bb1f0bd3
prachi dwivedi <131534182+prachi137@users.noreply.github.com>
2023-05-26 10:04:05 +0530

    Create read_line_by_line.py

---
+++ read_line_by_line.py
@@ -0,0 +1,14 @@
+L= ["prachi\n", "for\n", "dwivedi\n"]
+ 
+file1 = open('myfile.txt', 'w')
+file1.writelines(L)
+file1.close()
+ 
+
+file1 = open('myfile.txt', 'r')
+Lines = file1.readlines()
+ 
+count = 0
+for line in Lines:
+    count += 1
+    print("Line{}: {}".format(count, line.strip())
