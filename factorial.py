dc8191576179214ed5c8c06fa3a32df4b85b84e5
prachi dwivedi <131534182+prachi137@users.noreply.github.com>
2023-05-26 10:11:38 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,2 +1,15 @@
-# palindrome.py
-palindrome.py
+def factorial(n):
+     
+    if (n==1 or n==0):
+         
+        return 1
+     
+    else:
+    return (n * factorial(n - 1))
+num = 5;
+print("number : ",num)
+print("Factorial : ",factorial(num))
+
+output:
+  numer:5
+    factorial:120
